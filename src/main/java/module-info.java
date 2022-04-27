c6f44df8dcfbe1ce9be231bce5494c8b7846fa36
Michael Schmidt <development@hirola.de>
2022-04-27 19:31:45 +0200

    Release v.0.1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,10 @@ module de.hirola.runningplanbuilder {
     requires org.jetbrains.annotations;
     requires sportsapplications;
     requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
     requires org.tinylog.api;
+    requires org.tinylog.impl;
     requires java.prefs;
 
     opens de.hirola.runningplanbuilder.model to javafx.base;
